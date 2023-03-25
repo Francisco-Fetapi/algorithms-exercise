41229aa0082d0e62d0866611044162d9f3b769e8
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-03-25 01:43:19 +0100

    refactor: some exercises

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
-import { removeElement } from "./exercises/removeElement";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>

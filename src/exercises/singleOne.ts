97f16a4f03cd7f8d035b207f276ca59f48402eae
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-09 08:49:33 +0100

    feat: single one solved

--- src/exercises/singleOne.ts
+++ src/exercises/singleOne.ts
@@ -8,6 +8,8 @@ export function singleOne(nums: number[]): number {
     numbersCount[num] = (numbersCount[num] || 0) + 1;
   }
 
+  // TODO: ask for Chat GPT if can provide some solution with O(n) time complexity instead O(n * 2)
+
   for (let i = 0; i < nums.length; i++) {
     let num = nums[i];
     if (numbersCount[num] === 1) {

e4b8e721577d6f769402f012afeb25610848264c
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-15 17:22:45 +0100

    refactor: power of two

--- src/exercises/powerOfTwo.ts
+++ src/exercises/powerOfTwo.ts
@@ -1,6 +1,7 @@
 // Problem URL: https://leetcode.com/problems/power-of-two
 
 export function powerOfTwo(n: number): boolean {
+  if (n <= 0) return false;
   let logN2 = Math.log2(n);
 
   //   it is not a decimal number

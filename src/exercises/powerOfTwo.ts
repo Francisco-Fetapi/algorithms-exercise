2b947d0558613e823f7fb68216ccc0f8e9415f54
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-19 11:34:11 +0100

    refactor: powerOfTwo improved

--- src/exercises/powerOfTwo.ts
+++ src/exercises/powerOfTwo.ts
@@ -1,9 +1,22 @@
 // Problem URL: https://leetcode.com/problems/power-of-two
 
+/*
+// My first implementation
 export function powerOfTwo(n: number): boolean {
   if (n <= 0) return false;
   let logN2 = Math.log2(n);
 
   //   it is not a decimal number
   return !logN2.toString().includes(".");
+} */
+
+export function powerOfTwo(n: number): boolean {
+  if (n % 2 === 0) {
+    return powerOfTwo(n / 2);
+  }
+
+  if (n <= 0) return false;
+  else if (n === 1) return true;
+
+  return false;
 }

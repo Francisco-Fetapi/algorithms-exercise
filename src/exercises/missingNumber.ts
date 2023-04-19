a2fa0628df06b7e87fa010956b1422f5a57876e5
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-04-19 16:35:55 +0100

    missing number solved

--- src/exercises/missingNumber.ts
+++ src/exercises/missingNumber.ts
@@ -1,8 +1,9 @@
 // Problem URL: https://leetcode.com/problems/missing-number
 
 export function missingNumber(nums: number[]): number {
-  nums.sort((a, b) => a - b);
+  nums.sort();
 
+  //   TODO: solve with o(n) time complexity, use insertion sort only.
   for (let i = 0; i < nums.length - 1; i++) {
     let current = nums[i];
     let next = nums[i + 1];

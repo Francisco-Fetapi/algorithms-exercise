9f5e88039f731f59a5190e48a33d455eef288733
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-05-18 17:54:49 +0100

    feat: solving tictactoe

--- src/exercises/makeStraightLine.ts
+++ src/exercises/makeStraightLine.ts
@@ -1,3 +1,5 @@
+// Problem URL: https://leetcode.com/problems/check-if-it-is-a-straight-line
+
 export function makeStraightLine(coordinate: number[][]): boolean {
   return coordinate.every((xy) => xy[1] - xy[0] == 1);
 }
